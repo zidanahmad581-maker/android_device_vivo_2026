804fab81e542962f1c8d8c045ce3ecd6b2fc5a2b
zidanahmad581-maker <zidanahmad581@gmail.com>
2026-06-02 05:13:34 +0700

    Update and rename omni_2026.mk to twrp_2026.mk

--- twrp_2026.mk
+++ twrp_2026.mk
@@ -10,13 +10,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+#(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from 2026 device
-$(call inherit-product, device/vivo/2026/device.mk)
+#(call inherit-product, device/vivo/2026/device.mk)
 
 PRODUCT_DEVICE := 2026
-PRODUCT_NAME := omni_2026
+PRODUCT_NAME := twrp_2026
 PRODUCT_BRAND := vivo
 PRODUCT_MODEL := V2026
 PRODUCT_MANUFACTURER := vivo
